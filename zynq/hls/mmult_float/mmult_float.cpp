5551038469c296b34d594628f470e6408046221c
kdung <kimdung.bkit@gmail.com>
2019-04-04 22:38:18 +0800

    part 1E tiling

--- zynq/hls/mmult_float/mmult_float.cpp
+++ zynq/hls/mmult_float/mmult_float.cpp
@@ -28,9 +28,9 @@ void mmult_hw (AXI_VAL in_stream[IS_SIZE], AXI_VAL out_stream[OS_SIZE])
 	T offset_buf[CLASSES];
 	T weight_buf[CLASSES][FEAT];
 #pragma HLS ARRAY_PARTITION variable=weight_buf block factor=8 dim=2
-	T in_buf[BATCH][FEAT];
+	T in_buf[TILE_SIZE][FEAT];
 #pragma HLS ARRAY_PARTITION variable=in_buf block factor=8 dim=2
-	T out_buf[BATCH][CLASSES];
+	T out_buf[TILE_SIZE][CLASSES];
 
 	// Input and output AXI stream indices
 	int is_idx = 0;
@@ -56,39 +56,42 @@ void mmult_hw (AXI_VAL in_stream[IS_SIZE], AXI_VAL out_stream[OS_SIZE])
 
 
 	// Stream in input matrix
-	LOAD_I_1: for (int i = 0; i < BATCH; i++) {
-		LOAD_I_2: for (int j = 0; j < FEAT; j+=WIDTH_RATIO) {
-#pragma HLS PIPELINE II=1
-			// Pop AXI data packet
-			converter.packet = pop_stream(in_stream[is_idx++]);
-			in_buf[i][j+0]  = converter.val.f0;
-			in_buf[i][j+1]  = converter.val.f1;
-		}
-	}
-
-	// Iterate over batch elements
-	L1: for (int i = 0; i < BATCH; i++) {
-		// Iterate over output classes
-		L2: for (int j = 0; j < CLASSES; j++) {
-#pragma HLS PIPELINE II=1
-			// Perform the dot product
-			T tmp = offset_buf[j];
-			L3: for(int k = 0; k < FEAT; k++) {
-#pragma HLS PIPELINE II=1
-				tmp += in_buf[i][k] * weight_buf[j][k];
+	LT: for (int t = 0; t < BATCH; t += TILE_SIZE) {
+		LOAD_I_1: for (int i = 0; i < TILE_SIZE; i++) {
+			LOAD_I_2: for (int j = 0; j < FEAT; j+=WIDTH_RATIO) {
+	#pragma HLS PIPELINE II=1
+				// Pop AXI data packet
+				converter.packet = pop_stream(in_stream[is_idx++]);
+				in_buf[i][j+0]  = converter.val.f0;
+				in_buf[i][j+1]  = converter.val.f1;
 			}
-			out_buf[i][j] = tmp;
 		}
-	}
 
-	// Stream out output matrix
-	STORE_O_1: for (int i = 0; i < BATCH; i++) {
-		STORE_O_2: for (int j = 0; j < CLASSES; j+=WIDTH_RATIO) {
-#pragma HLS PIPELINE II=1
-			// Push output element into AXI stream
-			converter.val.f0 = out_buf[i][j+0];
-			converter.val.f1 = out_buf[i][j+1];
-			out_stream[os_idx++] = push_stream(converter.packet, os_idx == (OS_SIZE));
+
+		// Iterate over batch elements
+		L1: for (int i = 0; i < TILE_SIZE; i++) {
+			// Iterate over output classes
+			L2: for (int j = 0; j < CLASSES; j++) {
+	#pragma HLS PIPELINE II=1
+				// Perform the dot product
+				T tmp = offset_buf[j];
+				L3: for(int k = 0; k < FEAT; k++) {
+	#pragma HLS PIPELINE II=1
+					tmp += in_buf[i][k] * weight_buf[j][k];
+				}
+				out_buf[i][j] = tmp;
+			}
+		}
+
+		// Stream out output matrix
+		STORE_O_1: for (int i = 0; i < TILE_SIZE; i++) {
+			STORE_O_2: for (int j = 0; j < CLASSES; j+=WIDTH_RATIO) {
+	#pragma HLS PIPELINE II=1
+				// Push output element into AXI stream
+				converter.val.f0 = out_buf[i][j+0];
+				converter.val.f1 = out_buf[i][j+1];
+				out_stream[os_idx++] = push_stream(converter.packet, os_idx == (OS_SIZE));
+			}
 		}
 	}
 }
